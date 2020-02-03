472a308b79609b4407deca134763c0ed21013c34
JacobCromwell <thecoldsleep9@gmail.com>
2020-02-02 20:28:31 -0500

    connected to API successfully

--- scripts/mainPage.js
+++ scripts/mainPage.js
@@ -32,3 +32,23 @@ function updateElement() {
     elt.innerHTML = '1 UPDATED p TAG ' + secretMsg;
   }
 }
+
+function getUsers() {
+  var request = new XMLHttpRequest()
+
+  request.open('GET', 'http://localhost:3000/users', true)
+  request.onload = function() {
+    // Begin accessing JSON data here
+    var data = JSON.parse(this.response)
+
+    if (request.status >= 200 && request.status < 400) {
+      data.forEach(user => {
+        console.log(user.username)
+      })
+    } else {
+      console.log('error')
+    }
+  }
+
+  request.send()
+}
