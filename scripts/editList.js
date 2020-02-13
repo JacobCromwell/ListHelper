98e44279428030512adacf23ae0bc12ec25cfef7
JacobCromwell <thecoldsleep9@gmail.com>
2020-02-12 23:08:26 -0500

    minor styling change. Initial logic to call the getItem endpoint

--- scripts/editList.js
+++ scripts/editList.js
@@ -1,5 +1,21 @@
 //document.getElementById("editList").addEventListener("click", getLists);
+async function getItem(url = '') {
+    // Default options are marked with *
+    const response = await fetch(url, {
+      method: 'GET', // *GET, POST, PUT, DELETE, etc.
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    return await response.json(); // parses JSON response into native JavaScript objects
+  }
+  
+  function getItemWithId(list_id) {
+    console.log('Getting THEM lists!!!');
+    getItem('http://localhost:3000/item/' + list_id)
+      .then((data) => {
+        console.log('response data: ' + data); // JSON data parsed by `response.json()` call
+      });
+  }
 
-function getLists(){
-    alert('here I would keep my lists. IF I HAD ANY!');
-}
+document.querySelector('#editList').addEventListener('click', getItemWithId(1))
