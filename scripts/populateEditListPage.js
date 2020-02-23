94e6ca8560baa00379893415b9ec4072e1aca367
JacobCromwell <thecoldsleep9@gmail.com>
2020-02-23 10:42:57 -0500

    The Mark Item As Purchased and the Mark Item As Not purchased buttons now cause a styling change.

--- scripts/populateEditListPage.js
+++ scripts/populateEditListPage.js
@@ -1,29 +1,23 @@
 window.addEventListener('load', function load(event) {
-    console.log('>>1<<');
     var createButton = document.getElementById('populateMe');
     updateElement();
-    //createButton.addEventListener('load', function () { updateElement(); });
   });
-
-  //document.getElementById("myFrame").addEventListener("load", myFunction);
   
   function updateElement() {
-    console.log('>>2<<');
     let parsedSessionItems = getSessionItems(1);
     let populateParagraph = document.getElementById("populateMe");
     let populationString = '';
     let nonPurchasedItems = '';
     let purchasedItems = '';
     for (item of parsedSessionItems) {
-        //populationString += item.url;
         if(item.purchased === false){
           nonPurchasedItems += `
-          <div class="item">
+          <div class="viewEditItem notPurchasedItem" id=${item.id}>
               <img class="itemPicture" src="../images/tv.png" />
               <p><b>URL: </b>${item.url}</p>
               <div class="itemButton">
                   <a href="${item.url}"><button class="dfltbutton">Go To Site</button></a>
-                  <button class="dfltbutton">Mark As Purchased</button>
+                  <button class="dfltbutton" onClick="markItemAsPurchased(${item.id})">Mark As Purchased</button>
               </div>
           </div>
           `;
@@ -32,12 +26,12 @@ window.addEventListener('load', function load(event) {
             purchasedItems += `<p id="purchasedItemsDivider">-------------------Purchased Items-------------------</p>`
           }
           purchasedItems += `
-          <div class="itemPurchased">
+          <div class="viewEditItem purchasedItem" id=${item.id}>
               <img class="itemPicture" src="../images/tv.png" />
               <p><b>URL: </b>${item.url}</p>
               <div class="itemButton">
                   <a href="${item.url}"><button class="dfltbutton">Go To Site</button></a>
-                  <button class="dfltbutton">Not Purchased</button>
+                  <button class="dfltbutton" onClick="markItemAsNOTPurchased(${item.id})">Not Purchased</button>
               </div>
           </div>
           `;
@@ -48,8 +42,22 @@ window.addEventListener('load', function load(event) {
   }
   
   function getSessionItems(list_id) {
-    console.log('>>3<<');
     let sessionItems = sessionStorage.getItem(`listItems_${list_id}`);
     let parsedSessionItems = JSON.parse(sessionItems);
     return parsedSessionItems;
+  }
+
+  /*
+  TODO these functions need to call the PUT /item/:id API to update the purchased flag
+  */
+  function markItemAsPurchased(itemId) {
+    var element = document.getElementById(`${itemId}`);
+    console.log('element: ' + element);
+    element.classList.toggle("purchasedItem");
+  }
+  
+  function markItemAsNOTPurchased(itemId) {
+    var element = document.getElementById(`${itemId}`);
+    console.log('element: ' + element);
+    element.classList.toggle("purchasedItem");
   }
