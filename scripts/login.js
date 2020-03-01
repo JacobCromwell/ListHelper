7e53cd4a363f0cc0ed39f85b89055bd8e98bcdf9
JacobCromwell <thecoldsleep9@gmail.com>
2020-03-01 14:40:18 -0500

    basic functionality for view lists page

--- scripts/login.js
+++ scripts/login.js
@@ -1,17 +1,5 @@
-/*
-  TODO
-  need to pass in the actual list_id to getItemWithId instead of using hard-coded value
-*/
-
-/*
-let loginUsername = document.getElementById('loginUsername').value;
-let loginPassword = document.getElementById('loginPassword').value;
-
-console.log('>>>loginUsername: ' + loginUsername);
-console.log('>>>loginPassword: ' + loginPassword);
-*/
 async function attemptLogin(url = '') {
-    
+
     let loginUsername = document.getElementById('loginUsername').value;
     let loginPassword = document.getElementById('loginPassword').value;
 
@@ -20,29 +8,41 @@ async function attemptLogin(url = '') {
 
     let bodyData = {
         username: loginUsername,
-        password: loginPassword 
+        password: loginPassword
     }
-    
+
     // Default options are marked with *
     const response = await fetch(url, {
-      method: 'POST', // *GET, POST, PUT, DELETE, etc.
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(bodyData)
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(bodyData)
     });
     return await response.json(); // parses JSON response into native JavaScript objects
-  }
-  
+}
+
 function postUserLogin() {
     console.log('post user login clicked');
-  queryString = 'http://localhost:3000/users/login';
-  console.log('queryString: ' + queryString);
-  attemptLogin(queryString)
-    .then((data) => {
-      console.log('response data: ' + data); // JSON data parsed by `response.json()` call
-      //sessionStorage.setItem(`LoggedInUser`, JSON.stringify(data));
-    });
+    queryString = 'http://localhost:3000/users/login';
+    console.log('queryString: ' + queryString);
+    attemptLogin(queryString)
+        .then((data) => {
+            if(data.length > 0){
+                console.log('response data: ' + JSON.stringify(data[0]));
+                sessionStorage.setItem(`LoggedInUser`, JSON.stringify(data[0]));
+                window.location.href = './mainPage.html';
+            } else {
+                let loginSnackbar = document.getElementById('loginSnackbar');
+                loginSnackbar.innerHTML = 'Failed To Login';    
+            }
+        })
+        .catch(err => {
+            console.log('error: ' + err);
+            // set Error Snack bar notification
+            let loginSnackbar = document.getElementById('loginSnackbar');
+            loginSnackbar.innerHTML = 'Failed To Login';
+        });
 }
 
 document.querySelector('#submitLogin').addEventListener('click', postUserLogin);
