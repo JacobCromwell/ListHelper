a798bef56e8eac97397827284867bec238d4cc43
JacobCromwell <thecoldsleep9@gmail.com>
2020-03-01 17:30:29 -0500

    more cleanup

--- scripts/populateEditListPage.js
+++ scripts/populateEditListPage.js
@@ -1,20 +1,17 @@
 window.addEventListener('load', function load(event) {
-    var createButton = document.getElementById('populateMe');
-    let helperListInScopeId = sessionStorage.getItem(`HelperListInScope`);
-    getItemWithId(helperListInScopeId);
-  });
-  
-  function updateElement(sessionItems) {
-    //console.log('updateElement listScopeId: ' + helperListInScopeId)
-    //let parsedSessionItems = getSessionItems(helperListInScopeId);
-    let populateParagraph = document.getElementById("populateMe");
-    let populationString = '';
-    let nonPurchasedItems = '';
-    let purchasedItems = '';
-    // Create the html of the page
-    for (item of sessionItems) {
-        if(item.purchased === false){
-          nonPurchasedItems += `
+  let helperListInScopeId = sessionStorage.getItem(`HelperListInScope`);
+  getItemWithId(helperListInScopeId);
+});
+
+function updateElement(sessionItems) {
+  let populateParagraph = document.getElementById("populateMe");
+  let populationString = '';
+  let nonPurchasedItems = '';
+  let purchasedItems = '';
+  // Create the html of the page
+  for (item of sessionItems) {
+    if (item.purchased === false) {
+      nonPurchasedItems += `
           <div class="viewEditItem notPurchasedItem" id=${item.id}>
               <img class="itemPicture" src="../images/tv.png" />
               <p><b>URL: </b>${item.url}</p>
@@ -24,11 +21,11 @@ window.addEventListener('load', function load(event) {
               </div>
           </div>
           `;
-        } else if(item.purchased === true){
-          if(purchasedItems === ''){
-            purchasedItems += `<p id="purchasedItemsDivider">-------------------Purchased Items-------------------</p>`
-          }
-          purchasedItems += `
+    } else if (item.purchased === true) {
+      if (purchasedItems === '') {
+        purchasedItems += `<p id="purchasedItemsDivider">-------------------Purchased Items-------------------</p>`
+      }
+      purchasedItems += `
           <div class="viewEditItem purchasedItem" id=${item.id}>
               <img class="itemPicture" src="../images/tv.png" />
               <p><b>URL: </b>${item.url}</p>
@@ -38,44 +35,47 @@ window.addEventListener('load', function load(event) {
               </div>
           </div>
           `;
-        }
-    }
-    populationString += nonPurchasedItems + purchasedItems;
-    populationString += `<a href="mainPage.html"><button class="footButtons">Cancel</button></a>`
-    populateParagraph.innerHTML = populationString;
-
-    //create the event listeners
-    for(item of sessionItems){
-      console.log('itemId for eventListenerCreation: ' + item.id);
-      if(item.purchased === false){
-        document.getElementById(`markAsPurchasedButton${item.id}`).addEventListener("click", function() {markItemAsPurchased(item.id)});
-      } else if(item.purchased === true){
-        //document.getElementById(`markItemAsNOTPurchased${item.id}`).addEventListener("click", markItemAsNOTPurchased(item.id));
-      }
     }
   }
+  populationString += nonPurchasedItems + purchasedItems;
+  populationString += `<a href="mainPage.html"><button class="footButtons">Cancel</button></a>`
+  populateParagraph.innerHTML = populationString;
 
-  /*
-  TODO these functions need to call the PUT /item/:id API to update the purchased flag
-  */
-  function markItemAsPurchased(itemId) {
-    console.log('itemId: ' + itemId);
-    var element = document.getElementById(`${itemId}`);
-    console.log('element: ' + element);
-    element.classList.toggle("purchasedItem");
+  //create the event listeners
+  for (item of sessionItems) {
+    console.log('itemId for eventListenerCreation: ' + item.id);
+    if (item.purchased === false) {
+      document.getElementById(`markAsPurchasedButton${item.id}`).addEventListener("click", function () { markItemAsPurchased(item.id) });
+    } else if (item.purchased === true) {
+      //document.getElementById(`markItemAsNOTPurchased${item.id}`).addEventListener("click", markItemAsNOTPurchased(item.id));
+    }
   }
-  
-  function markItemAsNOTPurchased(itemId) {
-    var element = document.getElementById(`${itemId}`);
-    console.log('element: ' + element);
-    element.classList.toggle("purchasedItem");
-  }
+}
+
+/*
+TODO these functions need to call the PUT /item/:id API to update the purchased flag
+*/
+function markItemAsPurchased(itemId) {
+  console.log('itemId: ' + itemId);
+  var element = document.getElementById(`${itemId}`);
+  console.log('element: ' + element);
+  element.classList.toggle("purchasedItem");
+}
+
+function markItemAsNOTPurchased(itemId) {
+  var element = document.getElementById(`${itemId}`);
+  console.log('element: ' + element);
+  element.classList.toggle("purchasedItem");
+}
 
 
-  /*
-  -------------- QUERY DB For List of Items-----------------
-  */
- async function getItem(url = '') {
+// JRC TODO all of these calls to the API should be in a separate service
+/*
+-------------- QUERY DB GET Helper List Items-----------------
+helper_lists/${helperListInScopeId}/items/
+--------------------------------------------------------------
+*/
+async function getItem(url = '') {
   // Default options are marked with *
   const response = await fetch(url, {
     method: 'GET', // *GET, POST, PUT, DELETE, etc.
@@ -87,12 +87,17 @@ window.addEventListener('load', function load(event) {
 }
 
 function getItemWithId(helperListInScopeId) {
-queryString = `http://localhost:3000/helper_lists/${helperListInScopeId}/items/`;
-console.log('queryString: ' + queryString);
-getItem(queryString)
-  .then((data) => {
-    //console.log('response data.items: ' + data.items); // JSON data parsed by `response.json()` call
-    sessionStorage.setItem(`listItems_${helperListInScopeId}`, JSON.stringify(data.items));
-    updateElement(data.items);
-  });
+  queryString = `http://localhost:3000/helper_lists/${helperListInScopeId}/items/`;
+  console.log('queryString: ' + queryString);
+  getItem(queryString)
+    .then((data) => {
+      sessionStorage.setItem(`listItems_${helperListInScopeId}`, JSON.stringify(data.items));
+      updateElement(data.items);
+    });
 }
+
+/*
+-------------- QUERY DB PUT Item Purchased flag-----------------
+helper_lists/${helperListInScopeId}/items/
+----------------------------------------------------------------
+*/
